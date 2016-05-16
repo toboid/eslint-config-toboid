a574ad1efd00a5505f0460312e1800d913a86367
toboid <toboid@users.noreply.github.com>
2016-05-16 21:23:02 +0100

    Export eslint config

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  env: {
+    node: true,
+    mocha: true
+  },
+  rules: {
+    'no-extra-semi': 2,
+    semi: 2,
+    strict: 2,
+    'space-before-function-paren': 2,
+    'no-var': 2,
+    'prefer-const': 2,
+    'eol-last': 2,
+    'no-multiple-empty-lines': [
+      2,
+      {max: 1}
+    ],
+    quotes: [2, 'single'],
+    'no-console': 0
+  }
+};
